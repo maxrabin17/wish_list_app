2f0ba100f9e0b45c4b590ede290605d64c2d4f50
maxrabin <max.rabin17@gmail.com>
2021-08-23 18:23:14 -0400

    Changed cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins '*'
+        origins 'http://localhost:3000', 'https://wishing-well-app.herokuapp.com/'
 
     resource '*',
         headers: :any,
