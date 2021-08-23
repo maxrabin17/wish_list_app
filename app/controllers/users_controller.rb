d48412f63f43263e0d4a9da34bb6e8f37504a3d0
maxrabin <max.rabin17@gmail.com>
2021-08-23 13:22:19 -0400

    Commit message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     def create
         user = User.create!(user_params)
         session[:user_id] = user.id
-        render json: {user: user, wishes: user.wishes}, status: :created
+        render json: user, status: :created
     end
 
     def show
