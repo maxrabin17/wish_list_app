341c1f8e6f13c3238d3d0ac6034e1a96f4325ec1
maxrabin <max.rabin17@gmail.com>
2021-08-23 14:12:24 -0400

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  namespace :api do
+    resources :groups
+    resources :wishes
+    resources :users
+  
+    get '/me', to: 'users#show'
+  
+    post '/login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
+  end
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   # resources :comments
-  resources :groups
-  resources :wishes
-  resources :users
-
-  get '/me', to: 'users#show'
-
-  post '/login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
 end
