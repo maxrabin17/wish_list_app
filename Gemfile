841f8f4744a823bae8062d2b01daa910e140ca26
maxrabin <max.rabin17@gmail.com>
2021-08-23 18:15:06 -0400

    Add same site cookie gem

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.0.2'
+gem 'rails_same_site_cookie', '~> 0.1.8'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.4'
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,9 @@ Rails.application.routes.draw do
     resources :users
   
     get '/me', to: 'users#show'
-  
     post '/login', to: 'sessions#create'
     delete 'logout', to: 'sessions#destroy'
   # end
 
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
-  # resources :comments
 end
