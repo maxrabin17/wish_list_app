f3a37c521a349b09cb4c45d332e8003ebb5dccf4
maxrabin <max.rabin17@gmail.com>
2021-08-24 14:03:49 -0400

    added route

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
-  # namespace :api do
-    resources :groups
-    resources :wishes
-    resources :users
+  resources :groups
+  resources :wishes
+  resources :users
   
-    get '/me', to: 'users#show'
-    post '/login', to: 'sessions#create'
-    delete 'logout', to: 'sessions#destroy'
-  # end
-
+  get '/me', to: 'users#show'
+  post '/login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
+
+
+
+# namespace :api do
+# end
+
+
